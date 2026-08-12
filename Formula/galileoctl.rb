3dbb1b2117b0854f73b49de70482cbd1df6551b8
Deepak Sharma <deepak@galileo.ai>
2026-08-12 20:08:33 +0530

    Update galileoctl to v0.8.2 (#15)
    Co-authored-by: Obine Adoh <obine@galileo.ai>

--- Formula/galileoctl.rb
+++ Formula/galileoctl.rb
@@ -8,20 +8,20 @@
 class Galileoctl < Formula
   desc "Galileo CLI for quick troubleshooting and log bundle collection"
   homepage "https://github.com/rungalileo/galileoctl"
-  version "0.7.3"
+  version "0.8.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rungalileo/galileoctl/releases/download/v0.7.3/galileoctl-darwin-arm64"
-      sha256 "17794ba9b624dcf082bfcaaf4fea3807c8c880e21d1faecb73ce2f52ee15bf87"
+      url "https://github.com/rungalileo/galileoctl/releases/download/v0.8.2/galileoctl-darwin-arm64"
+      sha256 "25d55665176eacf5bf8a3fe16fc399dc9b78f9d3b15e76eaf69f4120358e0589"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/rungalileo/galileoctl/releases/download/v0.7.3/galileoctl-linux-amd64"
-      sha256 "10ba587238f95f3dad676db117c6821be91ee3f88f25a67db585e6f2b770538b"
+      url "https://github.com/rungalileo/galileoctl/releases/download/v0.8.2/galileoctl-linux-amd64"
+      sha256 "6df3877cebe5a193d43eb6265cea3bbc4f897aadb9ec5334eda04894e4cf4ff7"
     end
   end
 
